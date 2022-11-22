b75936af40ccbb394273f0023931c51df0b312ac
jhseong <jhseong@businessinsight.co.kr>
2022-11-22 16:09:28 +0900

    Flutter에서 Multipart로 이미지 업로드 요청하는 부분과 연동

--- api.py
+++ api.py
@@ -30,16 +30,41 @@ def return_flutter_doc(name):
 
 @app.route('/run', methods=['POST'])
 def run():
-    data = request.data
-    np_data = np.frombuffer(data, dtype='float32').reshape((299, 299, 3))
 
-    int_np_data = np.array(np_data, dtype='int').reshape((299, 299, 3))
-    cv2.imwrite('./test/test_white_rt/1.png', int_np_data)
+    # 저장할 위치 <= 나중에는 랜덤으로 경로와 파일명 작성해서 이미지 저장하고 DB에 저장해야 함
+    save_file_nm = './test.jpg'
+
+    # 일반 Post로 전송했을 경우 데이터를 받는 코드(기존 클라이언트)
+    data = request.data
+
+    # 기본 POST가 아니어서 data가 빈 값이면 multipart로 처리
+    if data == b'':
+        # file이라는 이름으로 첨부된 파일이 있는지 확인
+        if 'file' in request.files:
+            # test라는 이름으로 추가된 데이터 있는지 확인
+            str_data = request.form['test']
+            print(str_data)
+            
+            # FileStorage 포맷으로 받아와 일단 로컬에 저장
+            file = request.files['file']
+            file.save('./test.jpg')
+
+            # 저장된 파일을 다시 읽어옴
+            filestr = cv2.cvtColor(cv2.imread(save_file_nm, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)[:, :, :3]
+            data = filestr
+    else:
+        # POST 방식이면 저장하고 추론 진행
+        data = np.frombuffer(data, dtype='float32')
+
+        int_np_data = np.array(data, dtype='int').reshape((299, 299, 3))
+        cv2.imwrite(save_file_nm, int_np_data)
+
+    np_data = data.reshape((299, 299, 3))
 
     np_data_nm = np_data / 255.0
     frame = np_data_nm.reshape((1, 299, 299, 3))
 
-    cls_list = []
+    cls_list = ['true']
 
     return jsonify({'result': 'ok', 'cls_list': cls_list})
 
