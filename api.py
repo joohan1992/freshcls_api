6c05c3c89862d7ee5e0627a3566268a710f735e7
jun <leejunhee1219@snu.ac.kr>
2022-11-27 19:42:31 +0900

    api edit

--- api.py
+++ api.py
@@ -1,12 +1,43 @@
-import sys
-
 from flask import Flask, request, jsonify, render_template, send_from_directory, json
 from flask_cors import CORS
 import numpy as np
 import cv2
 import db_connector
-
 import ssl
+
+
+from keras.models import load_model
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing import image
+import os
+from datetime import datetime
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+def iferror(item, err="error"):
+    try:
+        return item
+    except:
+        return err
+    
+def FindTopN(predict, N):
+    TopN={}
+    for j in range(len(predict)):
+        if len(TopN)<N:
+            TopN[label[str(j)]]=round(100*predict[j],2)
+        else:
+            if round(100*predict[j],2)>min(TopN.values()):
+                del(TopN[min(TopN, key=TopN.get)])
+                TopN[label[str(j)]]=round(100*predict[j],2)
+    TopN=dict(sorted(TopN.items(), key=lambda x: x[1], reverse=True))
+    return TopN
+def now():
+    now = datetime.now()
+    string = str(now).replace(":", "-")
+    return string[0:10]+"_"+string[11:22]
+
+
 
 app = Flask(__name__, template_folder='web')
 CORS(app, support_credentials=True)
@@ -35,42 +66,46 @@ def return_flutter_doc(name):
 @app.route('/run', methods=['POST'])
 def run():
 
-    # 저장할 위치 <= 나중에는 랜덤으로 경로와 파일명 작성해서 이미지 저장하고 DB에 저장해야 함
-    save_file_nm = './test.jpg'
-
-    # 일반 Post로 전송했을 경우 데이터를 받는 코드(기존 클라이언트)
+    # 파일명
+    filename=now()+'.jpg'
+    save_file_path = './request_image/'+filename
+    while os.path.isfile(save_file_path):
+        filename= filename.split(".jpg")[0]+"(1).jpg"
+        save_file_path = './request_img/'+filename
+    print(save_file_path)
+    # 다시조립
     data = request.data
-
-    # 기본 POST가 아니어서 data가 빈 값이면 multipart로 처리
-    if data == b'':
-        # file이라는 이름으로 첨부된 파일이 있는지 확인
-        if 'file' in request.files:
-            # test라는 이름으로 추가된 데이터 있는지 확인
-            str_data = request.form['test']
-            print(str_data)
+    data = np.frombuffer(data, dtype='float32').reshape((210, 280, 3))
+    data = np.array(data, dtype='uint8')
+    cv2.imwrite(save_file_path,data)
+    
+    query = "INSERT INTO img_data(date,time,resol_x,resol_y,file_path)"
+    query += f" VALUES(NOW() ,NOW() , {280} , {210}, '{save_file_path[1:]}' ) RETURNING image_no"
+    dbConn = db_connector.DbConn()
+    dbConn.insert(query=query)        
+    
             
-            # FileStorage 포맷으로 받아와 일단 로컬에 저장
-            file = request.files['file']
-            file.save('./test.jpg')
-
-            # 저장된 파일을 다시 읽어옴
-            filestr = cv2.cvtColor(cv2.imread(save_file_nm, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)[:, :, :3]
-            data = filestr
-    else:
-        # POST 방식이면 저장하고 추론 진행
-        data = np.frombuffer(data, dtype='float32')
-
-        int_np_data = np.array(data, dtype='int').reshape((299, 299, 3))
-        cv2.imwrite(save_file_nm, int_np_data)
-
-    np_data = data.reshape((299, 299, 3))
-
-    np_data_nm = np_data / 255.0
-    frame = np_data_nm.reshape((1, 299, 299, 3))
-
+    # 일반 Post로 전송했을 경우 데이터를 받는 코드(기존 클라이언트), xsize랑 ysize도 같이 받아야 함.
+    ## 중간코드 추후작성요망
+    
     cls_list = ['true']
+    query = "INSERT INTO infer_history(date, str_no, model_no, image_no, result1, result2, result3, infer_speed, time, feedback)"
+    query += f" VALUES(NOW() ,{0} , {0} , {dbConn.lastpick()} , 1, NULL , NULL, {23} ,NOW(), NULL) RETURNING infer_no"
+    dbConn.insert(query=query)    
+    
+    return jsonify({'result': 'ok', 'cls_list': cls_list, 'infer_no':dbConn.lastpick(id=0) })
 
-    return jsonify({'result': 'ok', 'cls_list': cls_list})
+@app.route('/infer_feedback', methods=['POST'])
+def infer_feedback():
+    
+
+    res= request.get_json()
+    
+    dbConn = db_connector.DbConn()
+    query = f"UPDATE infer_history SET feedback = {res['feedback']} WHERE infer_no = {res['infer_no']}"
+    dbConn.insert(query=query)    
+    
+    return jsonify({'result': 'ok thanksyou for feedback' })
 
 
 @app.route('/get_model', methods=['GET', 'POST'])
@@ -165,4 +200,4 @@ def test():
 if __name__ == "__main__":
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     ssl_context.load_cert_chain(certfile='server.crt', keyfile='server.key', password='1234')
-    app.run(host='0.0.0.0', port=5443, ssl_context=ssl_context, debug=True)
+    app.run(host='0.0.0.0', port=5564, ssl_context=ssl_context, debug=False)
